b47e01017a9dd4cb190d814982175c3d6557f760
Igor Tirsina <igor.tirsina@yahoo.com>
2021-04-24 10:21:29 -0500

    Alphabet

---
+++ DoWhileLoopIntro.java
@@ -0,0 +1,9 @@
+public class DoWhileLoopIntro {
+    public static void main(String[] args) {
+        char letter = 'a';
+        do {
+            System.out.print(letter + " ");
+            letter++;
+        } while (letter <= 'z');
+    }
+}
